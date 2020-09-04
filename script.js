0254476d7fd298dc98b4b107b473d8a0c027463e
sonamnatsakanyan <66310681+sonamnatsakanyan@users.noreply.github.com>
2020-09-04 15:14:02 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+/*
+var firstname = 'john';
+console.log(firstname);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var education;
+console.log(education);
+
+var education = "Bachelor";
+console.log(education);
+
+console.log('Hello World');
+
+
+
+this is a big comment
+
+
+age = 'twenty eight';
+firstname = 'Mickeal';
+
+console.log(firstname + ' ' + age);
+
+age = 'twenty eight';
+firstname = 'Mickeal';
+var isMarried;
+
+
+alert ('My first name is ' +  firstname  + ' and I am ' + age + '. Am I married? ' + isMarried)
+
+
+
+
+firstname = 'Jack';
+var lastname;
+
+
+var lastname = prompt('What is your lastname?');
+console.log(firstname + ' ' + lastname);
+
+var fullname, age
+console.log(fullname = 'John');
+console.log(age = 28);
+
+
+*/
+
+
+
+
+var TipCalculator = function(bill) {
+   var tip;
+    if (bill < 50);
+        return tip = bill * 0.2;
+    else if (bill >= 50 && bill < 200);
+        return tip = bill * 0.15;
+    else 
+        return tip = bill * 0.1;
+}
+consol.log (TipCalculator(70));
+s
